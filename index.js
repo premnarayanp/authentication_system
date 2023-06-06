8699557d034bc421315ee7d005da55bd0cfd7a84
premnarayanp <premnarayanpatel223@gmail.com>
2023-06-06 05:41:21 +0530

    Express layout

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ const app = express();
 // use post request  url
 app.use(express.urlencoded());
 
-
-
 //for json req/res
 app.use(express.json());
+
+// use static file 
+app.use(express.static('./assets'));
+
+// Set Up the view Engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// use express layouts
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+app.use('/', require('./routes'));
 
 app.listen(port, function(error) {
 
